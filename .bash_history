f2766165976374fdcf0cc77a808732ab0b6532b1
hsiaoting wang <hsiaoting.wang@gmail.com>
2012-09-19 11:34:54 +0800

    First commit. Include the Java h file in C and list the first function for a test.

---
+++ .bash_history
@@ -0,0 +1,9 @@
+exit
+cd
+ll
+vi parser.c
+swapon -s
+swapon -a
+ll
+vi parser.c 
+exit
---
+++ parser.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "com_xinguard_parse_NatFlowNative.h"
+
+char* JNICALL Java_com_xinguard_parse_NatFlowNative_setPath();
